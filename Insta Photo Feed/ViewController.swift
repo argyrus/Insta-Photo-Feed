e0989656748a74d1b50fda6a6f9fff8906784f3d
argyrus <gshivam07@gmail.com>
2017-12-16 20:29:22 +0530

    Adding Basic code structure as an Xcode project

---
+++ Insta Photo Feed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insta Photo Feed
+//
+//  Created by Shivam Gupta on 16/12/17.
+//  Copyright © 2017 Shivam Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
